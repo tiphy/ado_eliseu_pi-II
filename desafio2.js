df8d1c7565837f7b799f7216b4dfa57e142cd7a9
tiphany <111897484+tiphy@users.noreply.github.com>
2025-09-08 19:20:19 -0300

    25_08

---
+++ desafio2.js
@@ -0,0 +1,5 @@
+const carros = ["fiat", "volkswagen", "ford", "chevrolet"];
+
+for(carro of carros){
+    console.log(carro);
+}
---
+++ vetor.js
@@ -0,0 +1,8 @@
+const notas = [3, 6, 9];
+function Gmedia (notas){
+let media = 0;
+for (nts of nota){
+media += nts;}
+return (media/notas.lengh);}
+let Gmediamedi = media(notas);
+console.log(medi.toFixers(2));
